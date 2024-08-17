f43bf6a068a3e1db5f6dccc11bd13a7ae0596a4e
Anh Trinh <namanht@student.unimelb.edu.au>
2024-08-17 20:12:05 +0700

    ajdnq

--- public/hackafun.js
+++ public/hackafun.js
@@ -1,6 +1,4 @@
 import '../public/hackafun.css'
- 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
